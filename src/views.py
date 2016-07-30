76408ae5171962ba8c61118ca6ddd3c41e3233f3
Aaron Barber <asbarber@umich.edu>
2016-07-29 22:36:39 -0700

    Setting up index

--- src/views.py
+++ src/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+def index_get(request):
+	return render(request, 'index_challenge1.html')
+
+# Performs log in action
+def index_post(request):
+	key1 = request.POST['key1']
+
+	if key1 == "713760":
+		return render(request, 'index_challenge2.html')
+
+	return HttpResponseRedirect('/')
+
+
+	
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+	if request.method == 'GET':
+		return login_get(request)
+	
+	if request.method == 'POST':
+		return login_post(request)
